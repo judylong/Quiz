eae80b22a1a6ea02ece073a7f43f3ef0d7614210
Judy Long <long.judy92@gmail.com>
2016-06-09 11:49:00 -0400

    setup backbone

---
+++ app/assets/javascripts/quiz.js
@@ -0,0 +1,13 @@
+window.Quiz = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  Quiz.initialize();
+});
