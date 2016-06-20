aad44e4e493fa1634b69b6be425f4d6ba53f9170
Judy Long <long.judy92@gmail.com>
2016-06-19 21:29:01 -0400

    modify parameter wrapping on backbone side

--- app/assets/javascripts/models/question.js
+++ app/assets/javascripts/models/question.js
@@ -1,4 +1,7 @@
 Quiz.Models.Question = Backbone.Model.extend({
   urlRoot: '/api/questions',
-
-})
+  toJSON: function() {
+    var json = {question: _.clone(this.attributes)};
+    return json;
+  }
+});
